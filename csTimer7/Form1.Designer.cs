38291975b77c851e350cfcb3a3ac4fda0cc62e02
TakeoHomma <michaeltelk24@gmail.com>
2025-02-13 06:10:55 +0200

    7
    Average of 5

---
+++ csTimer7/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace csTimer
+{
+    partial class csTimer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(csTimer));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MistyRose;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1000, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(-13, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1000, 100);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Scramble";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.ForeColor = System.Drawing.Color.Black;
+            this.panel2.Location = new System.Drawing.Point(0, 95);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1000, 5);
+            this.panel2.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(-13, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1000, 200);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Timer";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Snow;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(896, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 28);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "next";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(165, 430);
+            this.label5.TabIndex = 4;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(14, 113);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(165, 40);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Time";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(408, 363);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(139, 47);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "mo3: -";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(408, 410);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(139, 47);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "ao5: -";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // csTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Snow;
+            this.ClientSize = new System.Drawing.Size(982, 553);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "csTimer";
+            this.Text = "csTimer - Professional Rubik\'s Cube Timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.csTimer_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ csTimer7/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace csTimer
+{
+    public partial class csTimer : Form
+    {
+        public Random rand = new Random();
+        public Stopwatch stopwatch = new Stopwatch();
+        public List<string> list1 = new List<string>();
+        public List<double> list2 = new List<double>();
+        public List<double> list3 = new List<double>();
+        public int count = 0;
+
+        public void scramble(Label label2)
+        {
+            int a = rand.Next(-5, 7);
+            int b = rand.Next(-5, 7);
+            int c = rand.Next(-5, 7);
+            int d = rand.Next(-5, 7);
+            int f = rand.Next(-5, 7);
+            int g = rand.Next(-5, 7);
+            int h = rand.Next(-5, 7);
+            int i = rand.Next(-5, 7);
+            int j = rand.Next(-5, 7);
+            int k = rand.Next(-5, 7);
+            int l = rand.Next(-5, 7);
+            int m = rand.Next(-5, 7);
+            int n = rand.Next(-5, 7);
+            int o = rand.Next(-5, 7);
+
+            label2.Text = "UR" + a.ToString() + " DR" + b.ToString() + " DL" + c.ToString() + " UL" + d.ToString() + " U" + f.ToString() + " R" + g.ToString() + " D" + h.ToString() + " L" + i.ToString() + " ALL" + j.ToString() + " y2" + " U" + k.ToString() + " R" + l.ToString() + " D" + m.ToString() + " L" + n.ToString() + " ALL" + o.ToString();
+        }
+        public void Save()
+        {
+            string solve = label3.Text;
+            list1.Insert(0, count + ") " + solve);
+            label5.Text = string.Join("\n", list1);
+            if (list1.Count >= 10)
+            {
+                list1.RemoveAt(9);
+            }
+        }
+        public csTimer()
+        {
+        InitializeComponent();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            scramble(label2);
+
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan time = stopwatch.Elapsed;
+            label3.Text = time.Minutes.ToString() + ":" + time.Seconds.ToString("D2") + "." + (time.Milliseconds / 10).ToString("D2");
+        }
+
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void mo3()
+        {
+            if (list1.Count >= 3)
+            {
+
+                if (list2.Count > 3)
+                {
+                    list2.RemoveAt(list2.Count - 1);
+                }
+                double average = list2.Average() / 1000.0;
+                label7.Text = "mo3: " + average.ToString("F2");
+            }
+        }
+
+        private void ao5()
+        {
+            if (list3.Count >= 5)
+            {
+                var five = list3.Take(5).ToList();
+                five.Sort();
+                var three = five.Skip(1).Take(3).ToList();
+
+                double average2 = three.Average() / 1000.0;
+                label8.Text = "ao5: " + average2.ToString("F2");
+
+                if (list3.Count > 5)
+                {
+                    list3.RemoveAt(list3.Count - 1);
+                } 
+            }
+        }
+
+        private void csTimer_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if ((e.KeyValue == (char)Keys.Space)) {
+
+                if (stopwatch.IsRunning)
+                {
+                    
+                    stopwatch.Stop();
+                    timer1_Tick(null, null); // Updating the timer
+                    count++;
+                    Save();
+                    var timeTaken = stopwatch.ElapsedMilliseconds;
+                    list2.Insert(0, timeTaken);
+                    list3.Insert(0, timeTaken);
+                    mo3();
+                    ao5();
+                    scramble(label2);
+
+                    
+
+                }
+                else
+                {
+                    if ((e.KeyValue == (char)Keys.Space))
+                    {
+                        stopwatch.Restart();
+
+                    }
+                }
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            scramble(label2);
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
